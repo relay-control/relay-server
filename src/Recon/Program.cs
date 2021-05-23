49e70bdaac2a5446bb1ebd570e93b762400b7d9a
Lombra <lombravia@gmail.com>
2021-05-23 17:35:34 +0200

    Merge stuff.

--- src/Recon/Program.cs
+++ src/Recon/Program.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Recon.Core;
 
 namespace Recon {
 	class Program {
 		static void Main(string[] args) {
-			ReconServer server = new ReconServer(new string[] { });
-			Task.Factory.StartNew(() => server.Run(), TaskCreationOptions.LongRunning);
+			var builder = new WebHostBuilder()
+				.UseKestrel((builderContext, options) => {
+					//options.Listen(IPAddress.Loopback, 32155);
+					//options.Configure(builderContext.Configuration.GetSection("Kestrel"));
+				})
+				.ConfigureLogging((hostingContext, logging) => {
+					logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+					logging.AddConsole();
+					logging.AddDebug();
+				})
+				//.UseDefaultServiceProvider((context, options) => {
+				//	 options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+				// });
+				.UseStartup<Startup>()
+				.UseUrls("http://0.0.0.0:32155/");
+			
+			host = builder.Build();
+			
+			Task.Factory.StartNew(() => host.Run(), TaskCreationOptions.LongRunning);
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
